e72bcefc7def349703428619c502f4143679996f
Adriano Ruiz Boldarini <ruiz@7cliques.com.br>
2022-05-04 13:02:18 -0300

    fix: layout

--- quasar-starwars/src/pages/FilmsSection.vue
+++ quasar-starwars/src/pages/FilmsSection.vue
@@ -1,42 +1,19 @@
 <template>
   <q-layout class="bg-primary">
-    <div class="bg-secondary text-white">
-      <q-toolbar>
-        <q-space />
-        <q-btn color="black" flat round dense icon="search" class="q-mr-xs" />
-      </q-toolbar>
-    </div>
-    <div class="bg-primary q-pa-md">
-      <p class="text-caption text-grey-7 text-center">
-        <span class="text-bold text-subtitle1">OMG! </span> STAR WARS
-      </p>
-      <div class="flex">
-        <p class="text-bold text-h6 q-mr-md cursor-pointer">FILMS</p>
-        <p
-          clickable
-          @click="$router.push('/People')"
-          class="text-h6 q-mr-md text-grey-7"
-        >
-          PEOPLE
-        </p>
-        <p class="text-h6 q-mr-md text-grey-7">SPECIES</p>
-      </div>
-    </div>
+    <Header />
+    <SubHeader />
 
-    <div class="flex justify-evenly bg-primary">
+    <Loading v-if="loading" />
+
+    <div v-else class="flex justify-start bg-primary">
       <template v-for="film in films" :key="film">
         <q-card
           clickable="true"
           @click="$router.push('/character')"
-          class="shadow-17 size-card q-mb-md cursor-pointer"
+          class="shadow-1 size-card q-mb-md cursor-pointer"
         >
           <q-card-section class="q-pa-sm">
-            <img
-              width="100"
-              height="120"
-              src="../assets/no-image.jpeg"
-              :alt="film.alt"
-            />
+            <img width="100" src="../assets/no-image.jpeg" :alt="film.alt" />
             <p class="text-center text-caption text-bold">
               {{ film.title }}
             </p>
@@ -50,32 +27,31 @@
 <script lang="ts">
 import { defineComponent, ref, onMounted } from 'vue';
 import api from '../services/api';
-
+import Header from 'components/molecules/Header.vue';
+import SubHeader from 'components/molecules/SubHeader.vue';
+import Loading from '../components/molecules/Loading.vue';
 export default defineComponent({
-  setup () {
+  components: { Header, SubHeader, Loading },
+  setup() {
     const films = ref([]);
-
+    const loading = ref(true);
     const fetchFilms = async () => {
-      await api
-        .get('/films')
-        .then(response => (films.value = response.data.results));
+      const response = await api.get('/films');
+      films.value = response.data.results;
+      loading.value = false;
     };
 
     onMounted(fetchFilms);
 
-    return { films };
-  }
+    return { films, loading };
+  },
 });
 </script>
 
 <style scoped>
-.shadow {
-  box-shadow: 5px 30px 50px 26px rgb(255, 195, 0, 0.3);
-}
-
 .size-card {
   width: 115px;
-  height: 180px;
+  margin: 5px;
   border-bottom-right-radius: 15px;
 }
 </style>
--- quasar-starwars/src/pages/PeopleSection.vue
+++ quasar-starwars/src/pages/PeopleSection.vue
@@ -1,44 +1,25 @@
 <template>
   <q-layout class="bg-primary">
-    <div class="bg-secondary text-white">
-      <q-toolbar>
-        <q-space />
-        <q-btn color="black" flat round dense icon="search" class="q-mr-xs" />
-      </q-toolbar>
-    </div>
-    <div class="bg-primary q-pa-md">
-      <p class="text-caption text-grey-7 text-center">
-        <span class="text-bold text-subtitle1">OMG! </span> STAR WARS
-      </p>
-      <div class="flex">
-        <p
-          clickable
-          @click="$router.push('/')"
-          class="text-h6 q-mr-md text-grey-7"
-        >
-          FILMS
-        </p>
-        <p class="text-bold text-h6 q-mr-md cursor-pointer">PEOPLE</p>
-        <p class="text-h6 q-mr-md text-grey-7">SPECIES</p>
-      </div>
-    </div>
+    <Header />
+    <SubHeader />
+
+    <Loading v-if="loading" />
 
     <div class="flex justify-evenly bg-primary">
       <template v-for="character in characters" :key="character">
         <q-card
           clickable="true"
           @click="showData(character.url)"
-          class="shadow-17 size-card q-mb-sm cursor-pointer"
+          class="shadow-1 size-card q-mb-sm cursor-pointer"
         >
           <q-card-section class="q-pa-sm">
             <img
               width="100"
-              height="120"
               src="../assets/no-image.jpeg"
               :alt="character.alt"
             />
             <p class="text-center text-caption text-bold">
-                {{ character.name }}
+              {{ character.name }}
             </p>
           </q-card-section>
         </q-card>
@@ -51,41 +32,41 @@
 import { defineComponent, onMounted, ref } from 'vue';
 import { useRouter } from 'vue-router';
 import api from '../services/api';
+import Header from 'components/molecules/Header.vue';
+import Loading from '../components/molecules/Loading.vue';
+import SubHeader from 'src/components/molecules/SubHeader.vue';
 
 export default defineComponent({
-  setup () {
+  components: { Header, SubHeader, Loading },
+  setup() {
     const characters = ref([]);
-    console.log(characters)
+    console.log(characters);
+    const loading = ref(true);
 
     const fetchCharacters = async () => {
-      await api.get('/people').then(response => {
-        characters.value = response.data.results;
-      });
+      const response = await api.get('/people');
+      characters.value = response.data.results;
+      loading.value = false;
     };
-
 
     const router = useRouter();
 
     const showData = (url: string) => {
-      const id = url.substring(url.length - 2, url.length - 1)
-     void router.push(`/person/${id}`)
-    }
+      const id = url.substring(url.length - 2, url.length - 1);
+      void router.push(`/person/${id}`);
+    };
 
     onMounted(fetchCharacters);
 
-    return { characters, showData };
-  }
+    return { characters, showData, loading };
+  },
 });
 </script>
 
 <style scoped>
-.shadow {
-  box-shadow: 5px 30px 50px 26px rgb(255, 195, 0, 0.3);
-}
-
 .size-card {
   width: 115px;
-  height: 180px;
+  margin: 5px;
   border-bottom-right-radius: 15px;
 }
 </style>
